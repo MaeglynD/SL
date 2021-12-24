4080f101eda8a0680ee17d8eb2503b74fd1340fe
MaeglynD <maeglyn.davies@gmail.com>
2021-12-24 02:41:24 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {},
+  settings: {
+    'import/core-modules': [
+      'vuetify',
+      'vuetify/es5/util/colors',
+    ],
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', '.'],
+        ],
+        extensions: ['.vue', '.js'],
+      },
+    },
+  },
+};
