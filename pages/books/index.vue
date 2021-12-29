4b7c0a2e6f94da5b2dfca565c37f809e867ff073
MaeglynD <maeglyn@spellingshed.com>
2021-12-29 05:31:38 +0000

    added book selection page

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -10,15 +10,48 @@
   </div>
 
   <!-- Book list -->
-  <div v-else>
-    books page
-    <v-btn
-      v-for="book in books"
-      :key="book.id"
-      @click="goToBook(book.url)"
-    >
-      {{ book.name }}
-    </v-btn>
+  <div
+    v-else
+    class="oeuvre"
+  >
+    <div class="oeuvre__gradient-overlay" />
+
+    <div class="oeuvre__header-container">
+      <div class="oeuvre__header">
+        Oeuvre
+        <div class="oeuvre__header-background-text">
+          Oeuvre
+        </div>
+      </div>
+    </div>
+
+    <div class="oeuvre__book-list">
+      <div
+        v-for="({ bookCoverUrl, name, desc, pages, url }, i) in books"
+        :key="`book-${i}`"
+        class="oeuvre__book-container"
+        @click="goToBook(url)"
+      >
+        <v-img-no-referrer
+          contain
+          transition="fade-transition"
+          :lazy-src="`${bookCoverUrl}=w200`"
+          :src="`${bookCoverUrl}=w500`"
+        />
+
+        <div class="oeuvre__book-name">
+          {{ name }}
+        </div>
+
+        <div class="oeuvre__book-desc">
+          {{ desc }}
+        </div>
+
+        <div class="oeuvre__book-page-count">
+          {{ (pages.length -2) * 2 }} pages
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -51,11 +84,18 @@ export default {
     if (!this.books.length) {
       await this.loadBooks();
     }
+
+    await Promise.allSettled(
+      this.books
+        .filter(({ pages }) => !pages)
+        .map(({ id }) => this.loadBookPages(id)),
+    );
   },
 
   methods: {
     ...mapActions([
       'loadBooks',
+      'loadBookPages',
     ]),
 
     goToBook(url) {
